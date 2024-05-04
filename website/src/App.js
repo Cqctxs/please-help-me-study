053b3c46f022858deddf94f87dbac0c57956779c
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-05-04 15:31:56 -0400

    updated gradient

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Homepage from './components/Homepage';
 import ProblemDisplay from './components/ProblemDisplay';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
