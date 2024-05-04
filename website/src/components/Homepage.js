6d4e788475144507a9f3e4a850d408e81defa1d2
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-05-04 19:48:43 -0400

    Starting working on download link

--- website/src/components/Homepage.js
+++ website/src/components/Homepage.js
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Homepage = () => {
     return (
-    <div className="title-wrapper">
-        <div class="inner">PLEASE, HELP ME</div>
-        <div class="inner gradient">STUDY</div>
+    <div>
+        <div className="title-wrapper">
+            <div class="inner">PLEASE, HELP ME</div>
+            <div class="inner gradient">STUDY!</div>
+        </div>
+        <div className="title-wrapper">
+        </div>
+        <div>
+            <div className="download-wrapper-top">
+                <div class="flex">If you can't be </div>
+                <div class="flex second gradient">productive</div>
+            </div>
+            <div className="download-wrapper-bottom">
+                <div>yourself, let us give you a</div>
+                <div class="gradient">helping hand</div>
+                <button class="inner"></button>
+            </div>
+        </div>
     </div>
     );
 }
