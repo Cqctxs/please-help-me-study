52d18dd8c015fe6b214206fd35d8d336d36fffac
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-05-04 12:47:30 -0400

    qwer

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
- 
-function App() {
- return (
-   <div className="App">
-     <h1>Blocker</h1>
- 
-     <div className="SEOForm">
-       <h2>Add this site</h2>
-        <form>
-          <label>
-            Site URL:
-            <input type="url" name="url" value="temp url"/*{window.location.href}*/ required />
-          </label>
-          <button type="submit">Add</button>
-        </form>
-     </div>
-   </div>
- );
+
+function App(link) {
+  const [val, setVal] = useState("");
+  return (
+    <div className="App">
+      <h1>Blocker</h1>
+  
+      <div className="SEOForm">
+        <h2>What topic do you want to study?</h2>
+          <form>
+            <label>
+            <input type="text"
+            name="subject"
+            onChange = {(e) => {
+              setVal(e.target.value);
+              e.preventDefault();
+            }}
+            value={val}
+            required />
+            </label>
+            {/* <label>
+              Site URL:
+              <input type="url" name="url" value="temp url"{window.location.href} required />
+            </label>
+            <button type="submit">Add</button> */}
+          </form>
+      </div>
+    </div>
+  );
 }
  
 export default App;
