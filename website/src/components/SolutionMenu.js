df539738adbb53cc3ae3470f6d1a79017ad874ed
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-05-04 15:12:45 -0400

    Allowed for the TinyMCE form to be submitted

--- website/src/components/SolutionMenu.js
+++ website/src/components/SolutionMenu.js
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
+import axios from "axios";
 
 const SolutionMenu = () => {
-    return (
-        <Editor
-            apiKey='fm5ufxn5p7tojmfbzvqc6fxs62argkafj4p5cyb26ze8y5up'
-            init={{
-            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
-            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-            tinycomments_mode: 'embedded',
-            tinycomments_author: 'Author name',
-            mergetags_list: [
-                { value: 'First.Name', title: 'First Name' },
-                { value: 'Email', title: 'Email' },
-            ],
-            ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-            }}
-            initialValue="Welcome to TinyMCE!"
-        />
-    );
-}
+    const [content, setContent] = useState("");
 
-export default SolutionMenu
+    const handleEditorChange = (content) => {
+        setContent(content);
+    };
+
+    const handleSubmit = () => {
+        const rawText = document.createElement("div");
+        rawText.innerHTML = content;
+        const text = rawText.textContent || rawText.innerText;
+    
+        console.log(text);
+        // Post to server
+        // axios.post("/path/to/server.php", { text })
+        //     .then((response) => {
+        //         // Handle the response from the server
+        //         console.log(response)
+        //     })
+        //     .catch((error) => {
+        //         // Handle any errors
+        //         console.log(error)
+        //     });
+    };    
+
+    return (
+        <div>
+            <Editor
+                apiKey="fm5ufxn5p7tojmfbzvqc6fxs62argkafj4p5cyb26ze8y5up"
+                init={{
+                    selector: "textarea",
+                    plugins: "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
+                    toolbar: "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+                    tinycomments_mode: "embedded",
+                    tinycomments_author: "Author name",
+                    mergetags_list: [
+                        { value: "First.Name", title: "First Name" },
+                        { value: "Email", title: "Email" },
+                    ]
+                }}
+                initialValue="Enter your solution here."
+                onEditorChange={handleEditorChange}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    );
+};
+
+export default SolutionMenu;
