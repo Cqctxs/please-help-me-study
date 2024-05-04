fd9100f3cb57be092b3b9b744092d93f0b2b2cb5
Sean <seanzhao2007@gmail.com>
2024-05-04 09:38:43 -0400

    updated problem controller to get more consistent json responses

--- server/controllers/problemController.js
+++ server/controllers/problemController.js
@@ -6,7 +6,7 @@ const generate = async (req, res) => {
     return res.status(400).json({ error: "topic is required" });
   }
   const response = await ai.generateContent(
-    `Create a fun problem about ${topic} that you think would be interesting to solve. Make sure to include the problem and the solution in JSON formatting. It should look something like this: { "problem": "How many moles of oxygen is required to have a total mass of 16.00 grams?", "solution": "The answer is 1 mole. This is because the molar mass of oxygen is 16.00 g/mol, so multiplying by 1 mol will give 16.00 grams" }`
+    `Create a fun problem about ${topic} that you think would be interesting to solve. Make sure to include the problem and the solution in JSON formatting. It should look something like this: { "problem": "How many moles of oxygen is required to have a total mass of 16.00 grams?", "solution": "The answer is 1 mole. This is because the molar mass of oxygen is 16.00 g/mol, so multiplying by 1 mol will give 16.00 grams" } `
   );
   res.status(201).json({ response });
 };
