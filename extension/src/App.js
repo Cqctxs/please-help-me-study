dff9cd277097e5b88c60ec8c5fd590bd2f01b1a9
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-05-04 17:01:26 -0400

    started working on whitelist and blacklist

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+/* global chrome */
+import React, { useState } from 'react';
 import './App.css';
 import useChromeStorage from './hooks/useChromeStorage';
+import useChromeStoreList from './hooks/useChromeStoreList';
 
 function App(link) {
   const [val, setVal] = useChromeStorage("topic", "Mathematics");
+  const [url_w, set_url_w] = useState("");
+  const [url_b, blacklist] = useState("");
+
+  const [w, whitelist] = useChromeStoreList("whitelist", []);
+
   return (
     <div className="App">
       <h1>Blocker</h1>
     <form>
       <label>
+        <span>Today, I want to study </span>
         <input
           type="text"
           name="subject"
@@ -16,6 +24,25 @@ function App(link) {
             setVal(e.target.value);
           }}
           value={val}
+          required
+        />
+      </label>
+    </form>
+    <form>
+      <label>
+        <span> Add </span>
+        <input
+          type="text"
+          name="url"
+          placeholder="https://www.example.com/"
+          value = {url_w}
+          onChange={(e) => {
+            set_url_w(e.target.value);
+          }}
+          onSubmit={(e) => {
+            whitelist(url_w);
+            set_url_w("");
+          }}
           required
         />
       </label>
---
+++ extension/src/hooks/useChromeStoreList.js
@@ -0,0 +1,28 @@
+/* global chrome */
+
+import { useState, useEffect } from 'react';
+
+function useChromeStoreList(key, initialValue) {
+    const [storedValue, setStoredValue] = useState(initialValue);
+
+    useEffect(() => {
+        chrome.storage.sync.get([key], function(result) {
+            setStoredValue(result[key] ? JSON.parse(result[key]) : initialValue);
+        });
+    }, [key, initialValue]);
+
+    const appendValue = (value) => {
+        try {
+            const updatedValue = [...storedValue, value];
+            setStoredValue(updatedValue);
+            chrome.storage.sync.set({ [key]: JSON.stringify(updatedValue) });
+            console.log(updatedValue);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return [storedValue, appendValue];
+}
+
+export default useChromeStoreList;
