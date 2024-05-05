cb3f189d32e32f4f3057b2e82565aa6205b39642
Sean <seanzhao2007@gmail.com>
2024-05-05 01:52:01 -0400

    caddy ip

--- website/src/components/SolutionMenu.js
+++ website/src/components/SolutionMenu.js
@@ -20,7 +20,7 @@ const SolutionMenu = (solution, setSolution) => {
     console.log(text);
 
     // Send the solution to the server
-    fetch("https://api.pleasehelpme.study/api/problem/answer", {
+    fetch("34.31.69.199/api/problem/answer", {
       mode: "no-cors",
       method: "POST",
       headers: {
