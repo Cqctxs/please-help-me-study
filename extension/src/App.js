ff1a5d714287a5f3e1665a519867a0e57f7290db
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-05-05 01:55:57 -0400

    worked on extension css

--- extension/src/App.js
+++ extension/src/App.js
@@ -9,49 +9,37 @@ function App() {
   const [url_b, set_url_b] = useState("");
   const [w, whitelist] = useChromeStoreList("whitelist", []);
   const [b, blacklist] = useChromeStoreList("blacklist", []);
-  const [isWhitelistOpen, setIsWhitelistOpen] = useState(false);
-  const [isBlacklistOpen, setIsBlacklistOpen] = useState(false);
-
-  const toggleWhitelistDropdown = () => {
-    setIsWhitelistOpen(!isWhitelistOpen);
-  };
-
-  const toggleBlacklistDropdown = () => {
-    setIsBlacklistOpen(!isBlacklistOpen);
-  };
 
   return (
     <div className="App">
       <div className="field">
         <form>
-          <label className="fields">
-            <div>Today, I want to study</div>
-            <input
-              type="text"
-              name="subject"
-              onChange={(e) => {
-                setVal(e.target.value);
-              }}
-              value={val}
-              required
-            />
-          </label>
+          <h3>Today, I want to study</h3>
+          <input
+            type="text"
+            name="subject"
+            onChange={(e) => {
+              setVal(e.target.value);
+            }}
+            value={val}
+            required
+          />
         </form>
         <form>
-          <label className="fields">
-            <div>Whitelist</div>
-            <input
-              type="text"
-              name="url"
-              placeholder="https://www.example.com/"
-              value={url_w}
-              onChange={(e) => {
-                set_url_w(e.target.value);
-              }}
-              required
-            />
-          </label>
+          <h3>Whitelist</h3>
           <input
+            type="text"
+            name="url"
+            placeholder="https://www.example.com/"
+            value={url_w}
+            onChange={(e) => {
+              set_url_w(e.target.value);
+            }}
+            required
+          />
+        </form>
+        <div className="buttons">
+          <input className="submitButton"
             type="submit"
             value="Add"
             onClick={(e) => {
@@ -59,41 +47,32 @@ function App() {
               e.preventDefault();
             }}
           ></input>
-          <input
+          <input className="deleteButton"
             type="submit"
             value="Delete"
             onClick={(e) => {
               whitelist(url_w, "delete");
               set_url_w("");
               e.preventDefault();
-            }}
-          ></input>
-          <button onClick={toggleWhitelistDropdown}>
-            {isWhitelistOpen ? "Close" : "Open"} Whitelist Dropdown
-          </button>
-          {isWhitelistOpen && (
-            <div>
-              {w.map((item, index) => (
-                <div key={index}>{item}</div>
-              ))}
-            </div>
-          )}
-        </form>
+          }}>
+          </input>
+        </div>
+        <div className="list">{w.join(', ')}</div>
         <form>
-          <label className="fields">
-            <div>Blacklist</div>
-            <input
-              type="text"
-              name="url"
-              placeholder="https://www.example.com/"
-              value={url_b}
-              onChange={(e) => {
-                set_url_b(e.target.value);
-              }}
-              required
-            />
-          </label>
+          <h3>Blacklist</h3>
           <input
+            type="text"
+            name="url"
+            placeholder="https://www.example.com/"
+            value={url_b}
+            onChange={(e) => {
+              set_url_b(e.target.value);
+            }}
+            required
+          />
+        </form>
+        <div className="buttons">
+          <input className="submitButton"
             type="submit"
             value="Add"
             onClick={(e) => {
@@ -101,28 +80,17 @@ function App() {
               e.preventDefault();
             }}
           ></input>
-          <input
+          <input className="deleteButton"
             type="submit"
             value="Delete"
             onClick={(e) => {
               blacklist(url_b, "delete");
               set_url_b("");
               e.preventDefault();
-            }}
-          ></input>
-          <button onClick={toggleBlacklistDropdown}>
-            {isBlacklistOpen ? "Close" : "Open"} Blacklist Dropdown
-          </button>
-          {isBlacklistOpen && (
-            <div>
-              {b.map((item, index) => (
-                <div key={index}>{item}</div>
-              ))}
+            }}>
+            </input>
             </div>
-          )}
-        </form>
-        <div>{w}</div>
-        <div>{b}</div>
+        <div className="list">{b.join(', ')}</div>
       </div>
     </div>
   );
--- extension/src/hooks/useChromeStoreList.js
+++ extension/src/hooks/useChromeStoreList.js
@@ -13,22 +13,24 @@ function useChromeStoreList(key, initialValue) {
     }, [key, initialValue]);
     
     const update = (value, mode) => {
-        chrome.storage.sync.get([key], (obj) => {
-            let array = obj[[key]];
-            if (array && Array.isArray(array)) {
-                if (mode === "delete" && array.indexOf(value) !== -1) array.splice(array.indexOf(value), 1);
-                else if (mode === "add" && array.indexOf(value) === -1) array.push(value);
-                setUrlList(array);
-                chrome.storage.sync.set({
-                    [key]:array
-                }, function() {
-                    console.log("updated");
-                });
-            }
-            else {
-                console.log("Error: not an array");
-            }
-        });
+        if (value !== "" && value !== null && value !== undefined) {
+            chrome.storage.sync.get([key], (obj) => {
+                let array = obj[[key]];
+                if (array && Array.isArray(array)) {
+                    if (mode === "delete" && array.indexOf(value) !== -1) array.splice(array.indexOf(value), 1);
+                    else if (mode === "add" && array.indexOf(value) === -1) array.push(value);
+                    setUrlList(array);
+                    chrome.storage.sync.set({
+                        [key]:array
+                    }, function() {
+                        console.log("updated");
+                    });
+                }
+                else {
+                    console.log("Error: not an array");
+                }
+            });
+        }
     }    
 
     return [urlList, update];
