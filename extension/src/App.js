42d782ad704487d0e9e07524b406f35fa8b849bf
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-05-05 00:53:49 -0400

    Styling changes

--- extension/src/App.js
+++ extension/src/App.js
@@ -7,78 +7,125 @@ function App() {
   const [val, setVal] = useChromeStorage("topic", "Mathematics");
   const [url_w, set_url_w] = useState("");
   const [url_b, set_url_b] = useState("");
-
   const [w, whitelist] = useChromeStoreList("whitelist", []);
   const [b, blacklist] = useChromeStoreList("blacklist", []);
+  const [isWhitelistOpen, setIsWhitelistOpen] = useState(false);
+  const [isBlacklistOpen, setIsBlacklistOpen] = useState(false);
+
+  const toggleWhitelistDropdown = () => {
+    setIsWhitelistOpen(!isWhitelistOpen);
+  };
+
+  const toggleBlacklistDropdown = () => {
+    setIsBlacklistOpen(!isBlacklistOpen);
+  };
 
   return (
     <div className="App">
-      <h1>Blocker</h1>
-    <form>
-      <label>
-        <span>Today, I want to study </span>
-        <input
-          type="text"
-          name="subject"
-          onChange={(e) => {
-            setVal(e.target.value);
-          }}
-          value={val}
-          required
-        />
-      </label>
-    </form>
-    <form>
-      <label>
-        <span>Whitelist</span>
-        <input
-          type="text"
-          name="url"
-          placeholder="https://www.example.com/"
-          value = {url_w}
-          onChange={(e) => {
-            set_url_w(e.target.value);
-          }}
-          required
-        />
-      </label>
-      <input type="submit" value="Add" onClick={(e) => {
-      whitelist(url_w, "add");
-      e.preventDefault();
-    }}></input>
-      <input type="submit" value="Delete" onClick={(e) => {
-      whitelist(url_w, "delete");
-      e.preventDefault();
-    }}></input>
-    </form>
-    <form>
-      <label>
-        <span>Blacklist</span>
-        <input
-          type="text"
-          name="url"
-          placeholder="https://www.example.com/"
-          value = {url_b}
-          onChange={(e) => {
-            set_url_b(e.target.value);
-          }}
-          required
-        />
-      </label>
-      <input type="submit" value="Add" onClick={(e) => {
-      blacklist(url_b, "add");
-      e.preventDefault();
-    }}></input>
-      <input type="submit" value="Delete" onClick={(e) => {
-      blacklist(url_b, "delete");
-      e.preventDefault();
-    }}></input>
-    </form>
-    <div>{val}</div>
-    <div>{w}</div>
-    <div>{b}</div>
-  </div>
+      <div className="field">
+        <form>
+          <label className="fields">
+            <div>Today, I want to study</div>
+            <input
+              type="text"
+              name="subject"
+              onChange={(e) => {
+                setVal(e.target.value);
+              }}
+              value={val}
+              required
+            />
+          </label>
+        </form>
+        <form>
+          <label className="fields">
+            <div>Whitelist</div>
+            <input
+              type="text"
+              name="url"
+              placeholder="https://www.example.com/"
+              value={url_w}
+              onChange={(e) => {
+                set_url_w(e.target.value);
+              }}
+              required
+            />
+          </label>
+          <input
+            type="submit"
+            value="Add"
+            onClick={(e) => {
+              whitelist(url_w, "add");
+              e.preventDefault();
+            }}
+          ></input>
+          <input
+            type="submit"
+            value="Delete"
+            onClick={(e) => {
+              whitelist(url_w, "delete");
+              set_url_w("");
+              e.preventDefault();
+            }}
+          ></input>
+          <button onClick={toggleWhitelistDropdown}>
+            {isWhitelistOpen ? "Close" : "Open"} Whitelist Dropdown
+          </button>
+          {isWhitelistOpen && (
+            <div>
+              {w.map((item, index) => (
+                <div key={index}>{item}</div>
+              ))}
+            </div>
+          )}
+        </form>
+        <form>
+          <label className="fields">
+            <div>Blacklist</div>
+            <input
+              type="text"
+              name="url"
+              placeholder="https://www.example.com/"
+              value={url_b}
+              onChange={(e) => {
+                set_url_b(e.target.value);
+              }}
+              required
+            />
+          </label>
+          <input
+            type="submit"
+            value="Add"
+            onClick={(e) => {
+              blacklist(url_b, "add");
+              e.preventDefault();
+            }}
+          ></input>
+          <input
+            type="submit"
+            value="Delete"
+            onClick={(e) => {
+              blacklist(url_b, "delete");
+              set_url_b("");
+              e.preventDefault();
+            }}
+          ></input>
+          <button onClick={toggleBlacklistDropdown}>
+            {isBlacklistOpen ? "Close" : "Open"} Blacklist Dropdown
+          </button>
+          {isBlacklistOpen && (
+            <div>
+              {b.map((item, index) => (
+                <div key={index}>{item}</div>
+              ))}
+            </div>
+          )}
+        </form>
+        <div>{w}</div>
+        <div>{b}</div>
+      </div>
+    </div>
   );
 }
- 
+
 export default App;
