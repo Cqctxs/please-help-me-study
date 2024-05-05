5ff5e30264ee019e1c0d8fa041667eab4e4eeddc
lukas142434 <lukas.li@student.tdsb.on.ca>
2024-05-05 03:56:46 -0400

    Updated website to include link to github

--- website/src/components/Homepage.js
+++ website/src/components/Homepage.js
@@ -4,20 +4,19 @@ const Homepage = () => {
     return (
         <div>
             <div className="title-wrapper">
-                <div class="inner">PLEASE, HELP ME</div>
-                <div class="inner gradient">STUDY!</div>
+                <div className="inner">PLEASE, HELP ME</div>
+                <div className="inner gradient">STUDY!</div>
             </div>
             <div className="title-wrapper">
             </div>
             <div>
                 <div className="download-wrapper-top">
-                    <div class="flex">If you find being </div>
-                    <div class="flex second gradient2">productive</div>
+                    <div className="flex">If you find being </div>
+                    <div className="flex second gradient2">productive</div>
                 </div>
                 <div className="download-wrapper-bottom">
                     <div>hard, let us give you a</div>
-                    <div class="gradient">helping hand</div>
-                    <button class="inner"></button>
+                    <a href="https://github.com/Cqctxs/please-help-me-study" className="gradient">helping hand</a>
                 </div>
             </div>
         </div>
