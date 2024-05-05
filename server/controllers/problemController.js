a3fade29e11bafb1087eec54e90b83ebcaa127b0
Sean <seanzhao2007@gmail.com>
2024-05-05 03:05:11 -0400

    logging in server

--- server/controllers/problemController.js
+++ server/controllers/problemController.js
@@ -5,6 +5,7 @@ const generate = async (req, res) => {
   if (!topic) {
     return res.status(400).json({ error: "topic is required" });
   }
+  console.log(topic);
   const response = await ai.generateContent(
     `Create a fun problem about ${topic} that you think would be interesting to solve. Make sure to include the problem and the solution in JSON formatting. It should look something like this: { "problem": "How many moles of oxygen is required to have a total mass of 16.00 grams?", "solution": "The answer is 1 mole. This is because the molar mass of oxygen is 16.00 g/mol, so multiplying by 1 mol will give 16.00 grams" } `
   );
@@ -19,6 +20,7 @@ const answer = async (req, res) => {
   if (!solution) {
     return res.status(400).json({ error: "no solution recieved for the problem" });
   }
+  console.log(userResponse);
   const response = await ai.generateContent(
     `You are going now going to act as a teacher to grade a student's response to a problem. The solution is: ${solution}. The student's response is: ${userResponse}. Please give constructive criticism and a percentage grade. Use markdown format, and refrain from calling the student by "student" or "you".`
   );
